c16c2920ceb61c983f8f4003ae0fbbe99612921a
jomiguelmo <jose199523@gmail.com>
2026-03-05 20:41:45 -0500

    Add files via upload

---
+++ data/Rutas_SIF_3.js
@@ -0,0 +1,1 @@
+var json_Rutas_SIF_3 = {"type":"FeatureCollection","name":"Rutas_SIF_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/sif_areas_5.js
@@ -0,0 +1,1 @@
+var json_sif_areas_5 = {"type":"FeatureCollection","name":"sif_areas_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
